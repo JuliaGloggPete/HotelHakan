8f6745c7c5cb6f5760a67cf74338020e933a01b8
Tibalomium <33953814+Tibalomium@users.noreply.github.com>
2023-11-15 09:56:44 +0100

    Update index.js

---
+++ functions/deleteBooking/index.js
@@ -0,0 +1,52 @@
+//const AWS = require('aws-sdk');
+const { sendResponse } = require("../../responses/index");
+const dynamoDB = require("aws-sdk/clients/dynamodb");
+const db = new dynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+  const bookingID = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: "rooms-db",
+    };
+
+    const data = await db.scan(params).promise();
+
+    for (let key in data.Items) {
+      const item = data.Items[key];
+      if (item.booked.length > 0) {
+        const index = item.booked.findIndex(
+          (book) => book.bookingsnumber === "1699957478728"
+        );
+        if (index > -1) {
+          const updateParams = {
+            TableName: "rooms-db",
+            Key: {
+              id: item.id,
+            },
+            UpdateExpression: 'REMOVE booked[' + index + ']',
+          };
+          try {
+            await db.update(updateParams, (err) => {
+              if (err) {
+                
+              } else {
+                
+              }
+            }).promise();
+          } catch (err) { 
+            return sendResponse(500, {message: err});
+          }
+        }
+      }
+    }
+
+    return sendResponse(200, { success: true, message: "woop woop" }); //data.Items})
+  } catch (error) {
+    console.error("Error:", error);
+    return sendResponse(500, {
+      success: false,
+      message: "Misslyckades med att hämta rum från DynamoDB.",
+    });
+  }
+};
